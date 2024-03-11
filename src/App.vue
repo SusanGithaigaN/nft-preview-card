53e3f9fec326f7e9c2334ef30dc89749822a632b
susangithaiga <susangithaiga26@gmail.com>
2024-03-11 11:13:36 +0300

    /start

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+
+import NftCard from "./components/NftCard.vue";
+</script>
+
+<template>
+  <NftCard />
+</template>
+
+<style scoped></style>
---
+++ src/components/NftCard.vue
@@ -0,0 +1,10 @@
+<template>
+    <p>Demo</p>
+    </template>
+    
+    <script>
+    
+    </script>
+    
+    // <style scoped lang="scss"></style>
+    <style scoped></style>
